5c3e30fba3571e56f4c80125abf8b58bbfbc22ba
kimtani90 <kimtani90@gmail.com>
2017-04-06 18:17:25 -0700

    Modified files to update UI

--- src/components/ThumbNailSquare.js
+++ src/components/ThumbNailSquare.js
@@ -11,28 +11,25 @@ export default (props) => {
         <h3>Order At San jose</h3>
         <p>{props.sanjoseDescription}</p>
         <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
+          <Button bsStyle="primary">Button</Button>
         </p>
       </Thumbnail>
     </Col>
     <Col xs={6} md={4}>
       <Thumbnail src="https://s3-us-west-1.amazonaws.com/cmpe281starbuckscarouselimages/image1.jpg" alt="242x200">                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
         <h3>Order At San Francisco</h3>
-        <p>Description</p>
+        <p>{props.sanjoseDescription}</p>
         <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
+          <Button bsStyle="primary">Button</Button>
         </p>
       </Thumbnail>
     </Col>
     <Col xs={6} md={4}>
       <Thumbnail src="https://s3-us-west-1.amazonaws.com/cmpe281starbuckscarouselimages/image2.JPG" alt="242x200">                                                                                                                                                                                                                                                                                                                                                
         <h3>Order At Palo Alto</h3>
-        <p>Description</p>
+        <p>{props.sanjoseDescription}</p>
         <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
+          <Button bsStyle="primary">Button</Button>
         </p>
       </Thumbnail>
     </Col>
