8992d13b72a3b5c561d9e083844f860bc480346f
Nikhitha1234 <Nikhitha.ksr71@gmail.com>
2017-04-29 13:32:57 -0700

    Minor updates in Navigation Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Navbar, NavItem, Icon } from 'react-materialize';
 
+// <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
+// <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
+// <NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
+// <NavItem href='get-started.html'><Icon>more_vert</Icon></NavItem>
 export default () => {
   return (
     <Navbar brand='Starbucks' right>
-    <NavItem href='get-started.html'><Icon>search</Icon></NavItem>
-    <NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
-    <NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
-    <NavItem href='get-started.html'><Icon>more_vert</Icon></NavItem>
+    
     </Navbar>
   );
 };
+
